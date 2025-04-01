8003c493525b9188bceacf4671fd319ab25584a3
Maximus Wetterholm-Österman <maximus@oesterman.com>
2025-04-01 13:37:08 +0200

    exc1 done

--- big_seminar/exc1/main.cc
+++ big_seminar/exc1/main.cc
@@ -1,7 +1,25 @@
+#include <iostream>
+#include <set>
+#include <iomanip>
 
 int main()
 {
+    std::set<int> container{};
+    int input_int {};
 
+    while (std::cin >> input_int)
+    {
+        container.insert(input_int);
+    }
+
+    std::cout << std::endl;
+
+    for (int n : container)
+    {
+        std::cout << n << " ";
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
