96679e65425402d4b8e0ac983039326b6297b35c
maxri670 <maxri670@su17-213.ad.liu.se>
2025-04-01 14:21:19 +0200

    exc2 done

---
+++ big_seminar/exc2/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::vector<int> container{};
+    int input_int {};
+
+    while (std::cin >> input_int)
+    {
+        container.push_back(input_int);
+    }
+
+    std::cout << std::endl;
+
+    auto greatest_element {*std::max_element(container.begin(), container.end())};
+    auto count_of_greatest {std::count(container.begin(), container.end(), greatest_element)};
+
+    std::cout << count_of_greatest << std::endl;
+
+    return 0;
+}
