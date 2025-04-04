255ab10fde59ee142692087bb7cc8b7eb3affb8c
maxri670 <maxri670@su15-202.ad.liu.se>
2025-04-04 16:25:15 +0200

    print function works. Words from text stored in vector

--- lab1/main.cc
+++ lab1/main.cc
@@ -1,46 +1,73 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <map>
-#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <string>
 
-std::ifstream init_file(std::string const &filename)
+std::vector<std::string> init_file(std::string const &filename)
 {
     if (!std::filesystem::exists(filename))
     {
         throw std::runtime_error("File does not exists!");
     }
-    std::ifstream file(filename);
 
-    return file;
-}
-
-std::map<std::string, int> convert_to_map(std::ifstream const &file)
-{
-    std::map<std::string, int> words {};
-
-    words.insert()
-
-    //words.insert(file.begin(), file.end(), file.getline(words, file, " "));
+    std::ifstream ifs(filename);
+    std::vector<std::string> words {
+                std::istream_iterator<std::string>(ifs),
+                std::istream_iterator<std::string>()
+    };
+    
 
     return words;
-
 }
 
-void frequency(std::ifstream const &file)
-{
-    std::map<std::string, int> words { convert_to_map(file) };
+// std::vector<std::string> get_args(int argc, char* argv[])
+// {
+//     std::vector<std::string> arguments(argv + 1, argv + argc);
 
-    for ( const auto& [word, count]: words)
+//     if (arguments.size() == 0)
+//     {
+//         throw std::runtime_error("Too few arguments given!");
+//     }
+
+//     std::string filename { arguments[0] };
+
+//     return arguments;
+// }
+
+// std::map<std::string, int> convert_to_map(std::ifstream const &file)
+// {
+//     std::map<std::string, int> words {};
+
+//     words.insert()
+
+//     //words.insert(file.begin(), file.end(), file.getline(words, file, " "));
+
+//     return words;
+
+// }
+
+// void frequency(std::ifstream const &file)
+// {
+//     std::map<std::string, int> words { convert_to_map(file) };
+
+//     for ( const auto& [word, count]: words)
+//     {
+
+//         std::cout << word << " : " << count << std::endl;;
+//     }
+
+// }
+
+void print(std::vector<std::string> const &words)
+{
+    for (const auto& word : words)
     {
-        std::cout << word << " : " << count << std::endl;;
+        std::cout << word << " ";
     }
-
-}
-
-void print(std::ifstream const &file)
-{
-    std::cout << file.rdbuf();
+    std::cout << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -48,19 +75,21 @@ int main(int argc, char* argv[])
     std::vector<std::string> arguments(argv + 1, argv + argc);
     std::string filename { arguments[0] };
 
-    std::ifstream file { init_file(filename) };
+    std::vector<std::string> container{ init_file(filename) };
 
     for (std::string arg : arguments)
     {
         if (arg == "--print")
         {
-            print(file);
+            print(container);
         }
 
         else if (arg == "--frequency")
         {
             //frequency(file);
         }
+
+
     }
 
 
