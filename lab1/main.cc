3806c87fb92b331c360373f4752172f8f95990b9
maxri670 <maxri670@tlvm-4-2-2.ad.liu.se>
2025-04-23 14:12:53 +0200

    fixed duplication of code and other minor copying error

--- lab1/main.cc
+++ lab1/main.cc
@@ -8,6 +8,35 @@
 #include <map>
 #include <iomanip>
 
+
+
+//   Information om komplettering: 
+//   Kompletteringen kan gälla hela filen och alla filer i labben, 
+//   så får ni komplettering på en sak, kan samma sak förekomma på 
+//   fler ställen utan att jag skrivit det. 
+// 
+//   Komplettering lämnas in via sendlab inom 5 arbetsdagar. 
+//   Har ni frågor om kompletteringen kan ni maila mig. 
+
+//Komplettering: std::fixed används för flyttal. ---- DONE
+
+//Komplettering: Använd const& för att undvika onödig kopiering.(gäller även range-baserade for-loopar och lamdaparametrar) ----DONE
+
+//Komplettering: Duplicering av kod mellan frequency och table. -----DONE
+
+//Komplettering: Förklara hur det här fungerar.
+    // std::vector<std::string> words { //Chat.gpt helped with finding right algorithm
+    //             std::istream_iterator<std::string>(ifs),
+    //             std::istream_iterator<std::string>()
+    // };
+
+//Komplettering: Initiera variabler med måsvingar -----DONE
+
+//Komplettering: Fånga exceptions på lämpligt sätt. Användaren ska aldrig behöva se “Terminate called after...”.  
+
+//Kommentar: Felaktig hantering av undantag. Undantag bör endast kastas i exceptionella fall.
+
+
 std::vector<std::string> init_file(std::string const &filename)
 {
     if (!std::filesystem::exists(filename))
@@ -37,7 +66,7 @@ std::map<std::string, int> convert_to_map(std::vector<std::string> const &words)
 
 }
 
-std::string find_longest_word(std::vector<std::string> words) 
+std::string find_longest_word(std::vector<std::string> const &words) 
 {
     auto longest_word { std::max_element(words.begin(), words.end(),
         [](std::string const &a, std::string const &b)
@@ -55,7 +84,7 @@ std::string find_longest_word(std::vector<std::string> words)
 
 void print_pairs(std::vector<std::pair<std::string, int>> const& pairs, std::string const &longest_word)
 {
-    for(auto &[word, count] : pairs)
+    for(const auto &[word, count] : pairs)
     {
         std::cout << std::setw(longest_word.size())
                   << word
@@ -65,18 +94,34 @@ void print_pairs(std::vector<std::pair<std::string, int>> const& pairs, std::str
     }
 }
 
-void table(std::vector<std::string> const &words)
+void print_table_view(std::vector<std::string> const &words, bool frequency = false)
 {
-    std::map<std::string, int> map_of_words = convert_to_map(words);
+    std::map<std::string, int> map_of_words{ convert_to_map(words) };
     std::vector<std::pair<std::string, int>> wordpairs {};
 
     std::copy(map_of_words.begin(), map_of_words.end(), std::back_inserter(wordpairs));
 
-    std::string longest_word = find_longest_word(words);
+    std::string longest_word { find_longest_word(words) };
 
-    std::cout << std::fixed << std::left;
+    std::cout << std::left;
+
+    if(frequency)
+    {
+        std::sort(wordpairs.begin(), wordpairs.end(), 
+        [](std::pair<std::string, int> const &a, std::pair<std::string, int> const &b)
+        {
+            return a.second > b.second;
+        });
+
+        std::cout << std::right;
+    }
 
     print_pairs(wordpairs, longest_word);
+} 
+
+void table(std::vector<std::string> const &words)
+{
+    print_table_view(words, false);
 }
 
 void remove(std::vector<std::string> &words, std::string const &word_to_remove)
@@ -84,31 +129,16 @@ void remove(std::vector<std::string> &words, std::string const &word_to_remove)
     words.erase(std::remove(words.begin(),words.end(), word_to_remove), words.end());
 }
 
-
 void frequency(std::vector<std::string> const &words)
 {
-    std::map<std::string, int> map_of_words{ convert_to_map(words) };
-    std::vector<std::pair<std::string, int>> wordpairs {};
-
-    std::copy(map_of_words.begin(), map_of_words.end(), std::back_inserter(wordpairs));
-    std::sort(wordpairs.begin(), wordpairs.end(), 
-    [](std::pair<std::string, int> a, std::pair<std::string, int> b)
-    {
-        return a.second > b.second;
-    });
-
-    std::string longest_word = find_longest_word(words);
-
-    std::cout << std::fixed << std::right;
-
-    print_pairs(wordpairs, longest_word);
+    print_table_view(words, true);
 }
 
 void substitute(std::vector<std::string> &words, std::string const &old_word, std::string const &new_word)
 {
 
     std::transform(words.begin(), words.end(), words.begin(),
-            [&old_word, &new_word] (std::string current)
+            [&old_word, &new_word] (std::string const &current)
             {
                 
                 if (current == old_word)
@@ -148,7 +178,7 @@ int main(int argc, char* argv[])
     std::string filename { arguments[0] };
     std::vector<std::string> words{ init_file(filename) };
 
-    for (std::string arg : arguments)
+    for (const auto& arg : arguments)
     {
 
         if (arg == filename)
